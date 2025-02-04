9cfbbdda4e5055a3bc0e5cade49fdd5dec6d6468
Natalia Orlova <mad_tramp@mail.ru>
2025-02-04 23:01:07 +0400

    исправление ошибок

--- src/processing.py
+++ src/processing.py
@@ -8,5 +8,5 @@ def filter_by_state(operations: list, state: str = "EXECUTED") -> list:
 
 
 def sort_by_date(operations: list) -> list:
-    """Функция сортировки списка по дате"""
+    """Функцияяя сортировки списка по дате"""
     return sorted(operations, key=lambda operation: operation["date"], reverse=True)
