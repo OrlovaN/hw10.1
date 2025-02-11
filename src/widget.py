d7abd2566225b9e8bbac9d729085b079c373920a
Natalia Orlova <mad_tramp@mail.ru>
2025-02-11 14:01:10 +0400

    обновлены функции

--- src/widget.py
+++ src/widget.py
@@ -4,10 +4,12 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(account_data: str) -> str:
     """Функция маскировки данных счета или карты"""
     if "Счет" in account_data:
-        account_data_: str = "Счет " + get_mask_account(account_data[5:])
-    elif "Maestro" or "MasterCard" or "Visa Classic" or "Visa Platinum" or "Visa Gold" in account_data:
-        account_data_: str = account_data[0:-17] + " " + get_mask_card_number(account_data[-16:])
-    return account_data_
+        account_data: str = "Счет " + get_mask_account(account_data[5:])
+    elif "Maestro" in account_data or "MasterCard" in account_data or "Visa" in account_data:
+        account_data: str = account_data[0:-17] + " " + get_mask_card_number(account_data[-16:])
+    else:
+        raise TypeError("Неправильный формат")
+    return account_data
 
 
 def get_date(date: str) -> str:
